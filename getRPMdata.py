c7b42f05e8b42adf66bf61821bc9a74ec6c0b898
tom <tommy997865@msn.com>
2018-02-02 18:57:16 +0000

    Version with working current, but completely fucked rpm

--- getRPMdata.py
+++ getRPMdata.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 class rpmsensor():
 	def __init__(self):	
-		self.ser = serial.Serial(baudrate = 9600, port = '/dev/ttyUSB0', timeout = 0)
+		self.ser = serial.Serial(baudrate = 9600, port = '/dev/ttyACM0', timeout = 0)
 		self.rpm = -1
 		self.tim = -1
 	
@@ -14,12 +14,16 @@ class rpmsensor():
 		try:
 			while(self.ser.in_waiting):
 				incoming = self.ser.readline().rstrip()
-				splitUp = re.split(',', incoming)
-				self.rpm.decode(encoding = 'ascii', errors = 'ignore')
+				print incoming
+				
+				#self.rpm = int(incoming)
+				
 				self.tim = time
 		
 			return SensorData('4', 'rpm', self.rpm, self.tim)
 
 		except IndexError:
-			print "Wrong current value, nothing sent"			
+			print "Wrong RPM value, nothing sent"			
 
+		except ValueError:
+			print "wrong RPM value, could not cast to int"
